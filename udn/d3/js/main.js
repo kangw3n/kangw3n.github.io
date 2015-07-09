39885e61f5caf18176d8fb84303761e91ba59aa6
kangw3n <weitochearyeah@hotmail.com>
2015-07-09 14:20:06 +0800

    hide global fn

--- udn/d3/js/main.js
+++ udn/d3/js/main.js
@@ -2,7 +2,6 @@ var svg;
 var width = '800';
 var height = '800';
 var force; //dragging force layout
-var color = ['#0092C7', '#9FE0F6', '#F3E59A', '#F3B59B', '#F29C9C', '#7BA3A8', '#F4F3DE', '#BEAD92', '#F35A4A', '#6abf9c', '#7CC699', '#F3E4C2', '#F37A5A', '#EF4926']; //color hex for circle
 var svgCanvas = $('#idmap');
 var aspect = svgCanvas.width() / svgCanvas.height();
 var container = svgCanvas.parent();
@@ -41,7 +40,7 @@ $(document).ready(function() {
     .gravity(calculate.gravity(width))
     .size([width, height]);
 
-  ajaxCall(); // data get tru ajax json
+  _dataCall.ajaxCall(); // get init data tru ajax json
 });
 
 //click event
@@ -50,103 +49,107 @@ $('.sorts').on('click', function() {
 
   $('#idmap').empty();
 
-  ajaxCall(current);
+  _dataCall.ajaxCall(current); //get data by a specified query
 
 });
 
-function ajaxCall(arg) {
-  d3.json('data/newdata.json', function(error, root) {
-    var all = data(root, arg);
-    var tip = d3.tip()
-      .attr('class', 'd3-tip')
-      .offset([-10, 0])
-      .html(function(d) {
-        return '<strong>筆數一 ：</strong> <span class="tooltip-text">' + d.value + '</span> <br>' + '<strong>筆數二 ：</strong> <span class="tooltip-text">' + d.count + '</span> <br>';
-      });
+_dataCall = (function() {
 
-    if (error) throw error;
-    var node = svg.selectAll('.node')
-      .data(all.item)
-      .enter().append('a')
-      .attr('xlink:href', function(d) {
-        return d.link;
-      })
-      .append('g')
-      .classed('node', true)
-      .call(force.drag)
-      .call(tip);
+  var tip = d3.tip()
+    .attr('class', 'd3-tip')
+    .offset([-10, 0])
+    .html(function(d) {
+      return '<strong>筆數一 ：</strong> <span class="tooltip-text">' + d.value + '</span> <br>' +
+             '<strong>筆數二 ：</strong> <span class="tooltip-text">' + d.count + '</span> <br>';
+    }); //tooltips attr
 
-    var d3color = d3.scale.category20c();
+  var d3color = d3.scale.category20c();
+  var color = ['#0092C7', '#9FE0F6', '#F3E59A', '#F3B59B', '#F29C9C', '#7BA3A8', '#F4F3DE', '#BEAD92', '#F35A4A', '#6abf9c', '#7CC699', '#F3E4C2', '#F37A5A', '#EF4926']; //color hex for circle
 
-    //dynamic set the size of circle's radius
-    var rScale = d3.scale.linear()
-      .domain([d3.min(all.sumSize, function(d) {
-        if (width <= 200) {
-          return d / 1.5;
-        } else {
-          return d;
-        }
-      }), d3.max(all.sumSize, function(d) {
-        if (width <= 200) {
-          return d / 1.5;
-        } else {
-          return d;
-        }
-      })])
-      .range([width / 40, width / 10]); //set the sizeRange base of the size of svg
+  function draw(arg) {
+    d3.json('data/newdata.json', function(error, root) {
+      var all = data(root, arg);
 
-    node.append('circle')
-      .attr('r', function(d) {
-        return rScale(d.value);
-      })
-      .style('fill', color[Math.floor(Math.random() * 14)])
-      .transition()
-      .duration(1000)
-      .style({
-        'fill-opacity': 0.9,
-        stroke: 'rgba(255, 255, 255, .9)',
-        'stroke-width': 5,
-
-        //fill: color[Math.floor(Math.random() * 14)]
-
-        fill: function(d) {
-          if (rScale(d.value) >= 70) {
-            return color[1];
-          } else if (rScale(d.value) >= 45 && rScale(d.value) < 70) {
-            return color[2];
-          } else if (rScale(d.value) >= 25 && rScale(d.value) < 45) {
-            return color[3];
+      //dynamic set the size of circle's radius
+      var rScale = d3.scale.linear()
+        .domain([d3.min(all.sumSize, function(d) {
+          if (width <= 200) {
+            return d / 1.5;
           } else {
-            return color[4];
+            return d;
           }
-        }
-      });
+        }), d3.max(all.sumSize, function(d) {
+          if (width <= 200) {
+            return d / 1.5;
+          } else {
+            return d;
+          }
+        })])
+        .range([width / 40, width / 10]); //set the sizeRange base of the size of svg
 
-    node.append('text')
-      .on('mouseover', tip.show)
-      .on('mouseout', tip.hide)
-      .style({
-        'text-anchor': 'middle',
-        fill: 'black',
-        'font-size': function(d) {
-          return Math.floor(rScale(d.value) / 2) + 'px';
-        }
-      })
-      .attr('dy', '.35em')
-      .text(function(d) {
-        return d.className;
-      });
+      if (error) throw error;
+      var node = svg.selectAll('.node')
+        .data(all.item)
+        .enter().append('a')
+        .attr('xlink:href', function(d) {
+          return d.link;
+        })
+        .append('g')
+        .classed('node', true)
+        .call(force.drag)
+        .call(tip);
 
-    force.nodes(all.item)
-      .on('tick', dragging)
-      .start();
+      node.append('circle')
+        .attr('r', function(d) {
+          return rScale(d.value);
+        })
+        .style('fill', color[Math.floor(Math.random() * 14)])
+        .transition()
+        .duration(1000)
+        .style({
+          'fill-opacity': 0.9,
+          stroke: 'rgba(255, 255, 255, .9)',
+          'stroke-width': 5,
 
-    function dragging() {
-      node.attr('transform', function(d) {
-        return 'translate(' + d.x + ',' + d.y + ')';
-      });
-    } // transform location after drag
-  });
+          fill: function(d) {
+            if (rScale(d.value) >= 70) {
+              return color[1];
+            } else if (rScale(d.value) >= 45 && rScale(d.value) < 70) {
+              return color[2];
+            } else if (rScale(d.value) >= 25 && rScale(d.value) < 45) {
+              return color[3];
+            } else {
+              return color[4];
+            }
+          }
+        });
+
+      node.append('text')
+        .on('mouseover', tip.show)
+        .on('mouseout', tip.hide)
+        .style({
+          'text-anchor': 'middle',
+          fill: 'black',
+          'font-size': function(d) {
+            return Math.floor(rScale(d.value) / 2) + 'px';
+          }
+        })
+        .attr('dy', '.35em')
+        .text(function(d) {
+          return d.className;
+        });
+
+      force.nodes(all.item)
+        .on('tick', dragging)
+        .start();
+
+      function dragging() {
+        node.attr('transform', function(d) {
+          return 'translate(' + d.x + ',' + d.y + ')';
+        });
+      } // transform location after drag
+    });
+  }
 
   // Returns a flattened hierarchy containing all leaf nodes under the root.
   function data(root, arg) {
@@ -187,4 +190,8 @@ function ajaxCall(arg) {
     };
   }
 
-}
+  return {
+    ajaxCall: draw
+  };
+
+}());
