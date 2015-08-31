07c9311a109257039645de8f6937fc2f9588e7ae
kangw3n <weitochearyeah@hotmail.com>
2015-08-31 18:09:01 +0800

    css for time

--- udn/countdown/zh-cn.js
+++ udn/countdown/zh-cn.js
@@ -1,23 +1,23 @@
 (function($) {
-		
+
 	/**
 	 * FlipClock Chinese Language Pack
 	 *
 	 * This class will used to translate tokens into the Chinese language.
-	 *	
+	 *
 	 */
-	 
+
 	FlipClock.Lang.Chinese = {
-		
+
 		'years'   : '年',
 		'months'  : '月',
-		'days'    : '日',
+		'days'    : '天',
 		'hours'   : '时',
 		'minutes' : '分',
 		'seconds' : '秒'
 
 	};
-	
+
 	/* Create various aliases for convenience */
 
 	FlipClock.Lang['zh']      = FlipClock.Lang.Chinese;
